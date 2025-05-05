caab6714401dc937e6bdca6dfdaef969e2520518
T0nia <edtoniaelohor@gmail.com>
2025-05-05 14:08:38 +0100

    feat: used layer utilities to add background color

--- src/components/MainRectangle.jsx
+++ src/components/MainRectangle.jsx
@@ -1,17 +1,10 @@
-
 import React from 'react';
 
 const MainRectangle = () => {
   return (
-    <div className="absolute top-[45%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[800px] h-[400px]">
-      {/* Heading above the rectangle */}
-      <div className="absolute -top-9 left-1/2 transform -translate-x-1/2">
-        <h1 className="text-black text-2xl font-mono">Coinmap Currrency Converter</h1>
-      </div>
+<div className="w-4xl h-1/2 bg-panel-blue absolute top-20 left-60">
 
-      {/* The rectangle itself */}
-      <div className="w-full h-full bg-blue-600 border border-[#F8EDED]" />
-    </div>
+</div>
   );
 };
 
