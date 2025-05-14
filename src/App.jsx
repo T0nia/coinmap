2b0a0f3add807861c98280b2eed8c42097b34a6c
T0nia <edtoniaelohor@gmail.com>
2025-05-14 11:31:55 +0100

    added the heading text

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-
 import React from 'react';
 import MainRectangle from './components/MainRectangle';
 
 const App = () => {
   return (
     <div className="relative h-screen">
+      {/* Heading absolutely positioned to overlap the panel */}
+      <h1 className="absolute top-12 left-1/2 -translate-x-1/2 z-10 text-2xl text-black">
+        Coinmap Currency Converter
+      </h1>
+
+      {/* Main Rectangle Panel */}
       <MainRectangle />
     </div>
   );
